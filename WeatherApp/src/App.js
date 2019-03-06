0e3ec3ffc416bfb5553eed94d9d99d4cf04b0cca
Invidus <45075021+Invidus@users.noreply.github.com>
2019-03-06 22:45:42 +0300

    Add more fix

--- WeatherApp/src/App.js
+++ WeatherApp/src/App.js
@@ -13,22 +13,47 @@ import Form from './components/form';
 // }
 
 class App extends React.Component {
-    
+
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        error: undefined
+    }
+
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
-        const api_url = await
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=00c84f1f2e90aadb38b5b3daae21492f`);
-        const data = await api_url.json();
-        console.log(data);
 
+        if (city) {//Проверка на ввод инфы в поле
+            const api_url = await
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7e24578ea405cc1bc51c26e2e18df599`);
+            const data = await api_url.json();
+
+            var tempC = (data.main.temp - 32) *5 /9;
+
+            
+            this.setState({
+                temp: tempC,
+                city: data.name,
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                error: ""
+            });
+        }
     }
     render() {
         return (
             <div >
-                
+
                 <Form weatherMethod={this.getWeather} />
-                
+                <Weather
+                    temp={this.state.temp}
+                    city={this.state.city}
+                    country={this.state.country}
+                    pressure={this.state.pressure}
+                />
             </div>
         );
     }
--- WeatherApp/src/components/weather.jsx
+++ WeatherApp/src/components/weather.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const Weather = () => { 
-    debugger;
-    let getWeather = async () => {
-        debugger;
-        const api_url = await 
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=00c84f1f2e90aadb38b5b3daae21492f`)
-        const data = await api_url.json();
-        console.log(data);
-        alert("dsa");
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.country &&
+                    <div>
+                        {this.props.country}<br></br>
+                        {this.props.city}<br></br>
+                        {this.props.temp}<br></br>
+                        {this.props.pressure}<br></br>
+                    </div>
+                }
+
+            </div>
+
+        );
     }
 }
 
